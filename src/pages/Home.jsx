a287de1b036b3d9b36821c12e1187618158171a8
sumonDev9 <sumonmit678@gmail.com>
2025-06-05 17:39:38 +0530

    Implement pet grooming course details with YouTube embed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PetGroomingPromo from '../components/PetGroomingPromo';
 import WelcomeBanner from '../components/WelcomeBanner';
+import GroomingCourseInfo from '../components/GroomingCourseInfo';
 
 const Home = () => {
     return (
         <div>
           <PetGroomingPromo />
           <WelcomeBanner />
+          <GroomingCourseInfo />
         </div>
     );
 };
