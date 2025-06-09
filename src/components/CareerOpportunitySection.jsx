462273314129c7526f15fec93d2c2b84a92f628d
sumonDev9 <sumonmit678@gmail.com>
2025-06-09 12:17:05 +0530

    Add CareerOpportunitySection component with enrollment requirements

---
+++ src/components/CareerOpportunitySection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CareerOpportunitySection = () => {
+    return (
+        <div>
+            <div className='text-center space-y-4'>
+                <h1 className='pet-text text-black text-4xl md:text-5xl'>Enrollment Requirements</h1>
+                <p className='pet-text text-[#000000] text-2xl'>No prior experience is required</p>
+                <h2 className='pet-text text-black text-4xl md:text-5xl'>Career Opportunities</h2>
+            </div>
+        </div>
+    );
+};
+
+export default CareerOpportunitySection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,11 @@ import GroomingCourseInfo from '../components/GroomingCourseInfo';
 import OurProcesse from '../components/OurProcesse';
 import CourseModules from '../components/CourseModules';
 import CourseMethodology from '../components/CourseMethodology';
-import Happygraduates from '../components/Happygraduates';
-import FaqPetWelcome from '../components/FaqPetWelcome';
-import EarningOpportunityBanner from '../components/EarningOpportunityBanner';
-import CertificatePerson from '../components/CertificatePerson';
+// import Happygraduates from '../components/Happygraduates';
+// import FaqPetWelcome from '../components/FaqPetWelcome';
+// import EarningOpportunityBanner from '../components/EarningOpportunityBanner';
+// import CertificatePerson from '../components/CertificatePerson';
+import CareerOpportunitySection from '../components/CareerOpportunitySection';
 
 const Home = () => {
     return (
@@ -19,10 +20,11 @@ const Home = () => {
           <OurProcesse />
           <CourseModules />
           <CourseMethodology />
-          <Happygraduates />
+          <CareerOpportunitySection />
+          {/* <Happygraduates />
           <FaqPetWelcome />
           <CertificatePerson />
-          <EarningOpportunityBanner />
+          <EarningOpportunityBanner /> */}
         </div>
     );
 };
